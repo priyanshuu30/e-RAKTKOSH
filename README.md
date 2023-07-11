f6aa587787fe79139ff5e837acf9c7db5c311bab
Priyanshu Sekhar Pradhan <priyanshusekharpradhan3@gmail.com>
2023-07-11 11:06:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+
+	
+  <h1>Blood Donation Bank Management System</h1>
+	<p>This is a web-based Blood Donation Bank Management System project developed using HTML, CSS, JS, PHP, and MySQL.</p>
+<h2>Features</h2>
+<ul>
+	<li>User registration and login system</li>
+	<li>Donor and recipient management</li>
+	<li>Donation and blood request management</li>
+	<li>Admin dashboard for managing users, donors, recipients, donations, and blood requests.</li>
+</ul>
+
+<h2>Prerequisites</h2>
+<p>To run the project, you will need the following software installed on your machine:</p>
+<ul>
+	<li>XAMPP (or any other local web server stack)</li>
+	<li>PHP 7.4 or higher</li>
+	<li>MySQL 5.7 or higher</li>
+</ul>
+
+<h2>Installation</h2>
+<ol>
+	<li>Download or clone the project repository to your local machine.</li>
+	<li>Copy the downloaded project to the htdocs folder in your XAMPP installation directory (e.g., C:\xampp\htdocs).</li>
+	<li>Rename the downloaded project folder to <code>bdbms</code>. This will be the name of your project's root directory.</li>
+	<li>Open XAMPP and start Apache and MySQL.</li>
+	<li>Open a web browser and enter the following URL in the address bar: <code>http://localhost/e-RAKTKOSH/</code>.</li>
+</ol>
+
+<h2>Importing the Database</h2>
+<p>The database for this project has been created using phpMyAdmin. To import the database, follow these steps:</p>
+<ol>
+	<li>Open phpMyAdmin in your web browser by entering the following URL: <code>http://localhost/phpmyadmin/</code>.</li>
+	<li>Click on the "Import" tab in the top menu.</li>
+	<li>Click on the "Choose File" button and select the <code>bdbms.sql</code> file from the downloaded project.</li>
+	<li>Click on the "Go" button to import the database.</li>
+</ol>
+
+<h2>Credits</h2>
+<p>This project was developed as part of the Web Engineering course at UTKAL University by PRIYANSHU SEKHAR PRADHAN.</p>
+
+<p><strong>Note:</strong> If you encounter any issues while running the project or importing the database, please feel free to raise an issue in the GitHub repository. We would be happy to help you out!</p></html>
---
+++ about.php
@@ -0,0 +1,265 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Blood Bank Donar Management System | About Us </title>
+	<!-- Meta tag Keywords -->
+	
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--// Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<!-- //Web-Fonts -->
+
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+
+	<!-- banner 2 -->
+	<div class="inner-banner-w3ls">
+		<div class="container">
+
+		</div>
+		<!-- //banner 2 -->
+	</div>
+	<!-- page details -->
+	<!-- <div class="breadcrumb-agile">
+		<div aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active" aria-current="page">About Us</li>
+			</ol>
+		</div>
+	</div> -->
+	<!-- //page details -->
+
+	<!-- about -->
+	<section class="about">
+		<div class="container py-xl-2 py-lg-2">
+		<div class="w3ls-titles text-center mb-md-5 mb-4">
+				<h3 class="title">Who We Are?</h3>
+				<span>
+					<i class="fas fa-user-md"></i>
+				</span>
+				<p class="py-3">Our mission is to create a centralized platform that connects blood donors with those in need, making the process of blood donation more efficient and accessible. Through our website, we aim to promote awareness and educate the public on the importance of regular blood donation. By leveraging technology to streamline the process and connect donors with recipients in real-time, we hope to make a positive impact on the lives of those in need of life-saving blood transfusions.</p>
+			</div>		
+		</div>
+	</section>
+	
+	<!-- our teams -->
+	<section class="p-5">
+	<h3 class="title py-xl-2 py-lg-2">Behind This Project?</h3>
+	<div class="card-deck">
+	<div class="card">
+    <img src="images\ashu.jpeg" class="card-img-top team-img" alt="Ashu" style="height:26%">
+    <div class="card-body">
+      <h4 class="card-title">Ashutosh</h4>
+      <p class="card-text">Ashu is a valuable member of our team, bringing expertise in JavaScript and frontend technology as a final year Software Engineering student at Mehran University. With a passion for staying current and continuously learning, ashu is always seeking out new opportunities to expand his knowledge and skills.</p>      
+	  <div class="row p-2">
+	  <ul class="top-right-info">                            
+                                <li class="mx-3">
+                                    <a href="https://www.linkedin.com/in/ashutosh-kumar-pradhan-ab81a4240">
+                                        <i class="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/_mr_ashu08">
+                                        <i class="fab fa-instagram"></i>
+                                    </a>
+                                </li>                                
+                            </ul>
+	  </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="images\priyaaannn.jpeg" class="card-img-top team-img" alt="Priyaaannn" style="height:26%">
+    <div class="card-body">
+      <h4 class="card-title">Priyanshu</h4>
+      <p class="card-text">Priyanshu is a valuable team member with expertise in frontend technology. His dedication to growth and continuous learning makes him a valuable asset to any software development project. With a strong foundation in these areas, Priyanshu is well-equipped to make valuable contributions to the team.</p>      
+	  <div class="row p-2">
+	  <ul class="top-right-info">                            
+                                <li class="mx-3">
+                                    <a href="https://www.linkedin.com/in/priyanshu-sekhar-pradhan-449404216/">
+                                        <i class="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/__.priyanshu_sekhar.__">
+                                        <i class="fab fa-instagram"></i>
+                                    </a>
+                                </li>                                
+                            </ul>
+	  </div>
+    </div>
+  </div>
+  
+  <div class="card">
+    <img src="images\anup.jpeg" class="card-img-top team-img" alt="Anup" style="height:50%">
+    <div class="card-body">
+      <h4 class="card-title">Anup biswas</h4>
+      <p class="card-text">Anup is a valuable team member with expertise in frontend technology. His dedication to growth and continuous learning makes him a valuable asset to any software development project. With a strong foundation in these areas and a passion for staying current, anup is well-equipped to make valuable contributions to the team.</p>      
+	  <div class="row p-2">
+	  <ul class="top-right-info">                            
+                                <li class="mx-3">
+                                    <a href="https://www.linkedin.com/in/anup-biswas-5809b8222">
+                                        <i class="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/anup_biswas01">
+                                        <i class="fab fa-instagram"></i>
+                                    </a>
+                                </li>                                
+                            </ul>
+	  </div>
+    </div>
+  </div>
+  <div class="card">
+    <img src="images\mano.jpeg" class="card-img-top team-img" alt="mano" style="height:50%">
+    <div class="card-body">
+      <h4 class="card-title">Manojit</h4>
+      <p class="card-text">Mano is a valuable team member with expertise in frontend technology. His dedication to growth and continuous learning makes him a valuable asset to any software development project. With a strong foundation in these areas and a passion for staying current, mano is well-equipped to make valuable contributions to the team.</p>      
+	  <div class="row p-2">
+	  <ul class="top-right-info">                            
+                                <li class="mx-3">
+                                    <a href="https://www.linkedin.com/in/manojit-pattanayak-154352251">
+                                        <i class="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/man_o_jeet">
+                                        <i class="fab fa-instagram"></i>
+                                    </a>
+                                </li>                                
+                            </ul>
+	  </div>
+    </div>
+  </div>
+  <div class="card">
+    <img src="images\sai.jpeg" class="card-img-top team-img" alt="Sai" style="height:50%">
+    <div class="card-body">
+      <h4 class="card-title">Sai ranjan</h4>
+      <p class="card-text">Sai is a valuable team member with expertise in java, python and backend technology. His dedication to growth and continuous learning makes him a valuable asset to any software development project. With a strong foundation in these areas and a passion for staying current, sai is well-equipped to make valuable contributions to the team.</p>      
+	  <div class="row p-2">
+	  <ul class="top-right-info">                            
+                                <li class="mx-3">
+                                    <a href="https://www.linkedin.com/in/sai-ranjan-pradhan-724ba523b">
+                                        <i class="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/_s_a_i_ranjan">
+                                        <i class="fab fa-instagram"></i>
+                                    </a>
+                                </li>                                
+                            </ul>
+	  </div>
+    </div>
+  </div>
+  <div class="card">
+    <img src="images\chandu.jpeg" class="card-img-top team-img" alt="Chandu" style="height:120%">
+    <div class="card-body">
+      <h4 class="card-title">Chandu</h4>
+      <p class="card-text">Chandu is a valuable team member with expertise in HTML,CSS,Javascript and backend technology. His dedication to growth and continuous learning makes him a valuable asset to any software development project. With a strong foundation in these areas and a passion for staying current, chandu is well-equipped to make valuable contributions to the team.</p>      
+	  <div class="row p-2">
+	  <ul class="top-right-info">                            
+                                <li class="mx-3">
+                                    <a href="https://www.linkedin.com/in/chandrasekhar-bisoi">
+                                        <i class="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://instagram.com/__.chandra_sekhar.__">
+                                        <i class="fab fa-instagram"></i>
+                                    </a>
+                                </li>                                
+                            </ul>
+	  </div>
+    </div>
+  </div>
+</div>
+</section>
+	<!-- //about -->
+
+
+
+	<?php include('includes/footer.php');?>
+
+
+	<!-- Js files -->
+	<!-- JavaScript -->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- Default-JavaScript-File -->
+
+	<!-- banner slider -->
+	<script src="js/responsiveslides.min.js"></script>
+	<script>
+		$(function () {
+			$("#slider4").responsiveSlides({
+				auto: true,
+				pager: true,
+				nav: true,
+				speed: 1000,
+				namespace: "callbacks",
+				before: function () {
+					$('.events').append("<li>before event fired.</li>");
+				},
+				after: function () {
+					$('.events').append("<li>after event fired.</li>");
+				}
+			});
+		});
+	</script>
+	<!-- //banner slider -->
+
+	<!-- fixed navigation -->
+	<script src="js/fixed-nav.js"></script>
+	<!-- //fixed navigation -->
+
+	<!-- smooth scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<!-- move-top -->
+	<script src="js/move-top.js"></script>
+	<!-- easing -->
+	<script src="js/easing.js"></script>
+	<!--  necessary snippets for few javascript files -->
+	<script src="js/medic.js"></script>
+
+	<script src="js/bootstrap.js"></script>
+	<!-- Necessary-JavaScript-File-For-Bootstrap -->
+
+	<!-- //Js files -->
+
+</body>
+
+</html>
